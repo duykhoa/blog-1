708978bfedec2fd27f2cff0f3bd6608ced4d62cb
Sara <rrrrrah@gmail.com>
2014-12-15 20:10:33 -0500

    added gem will_paginate, fixed articles controller

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'devise'
 gem 'rmagick', '2.13.2'
 gem "paperclip", "~> 4.2"
 
+gem 'will_paginate', '~> 3.0.6'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,16 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article = Article.new(article_params)
+
+    if @article.published
+      @article.published_at = Time.now
+    end
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
 	end
 
 	def edit
@@ -16,13 +26,37 @@ class ArticlesController < ApplicationController
 
 	def update
 		@article = Article.find(params[:id])
+
+    was_published = @article.published
+
+    if @article.update(params[:article].permit(:title, :body, :published, :thumbnail))
+      if not was_published and @article.published
+        @article.published_at = Time.now
+        @article.save
+      end
+      redirect_to @article
+    else
+      render 'edit'
+    end
 	end
 
   def index
+    page_num = params[:page]
+    @article = Article.where(published: true).order(published_at: :desc).page(page_num)
+
+    if page_num.to_i > 1
+      @older_articles = @articles
+    else
+      @newest_article, *@older_articles = @article
+    end
   end
 
   def show
   	@article = Article.find(params[:id])
+
+    if not @article.published
+      authenticate_user!
+    end
   end
 
   def destroy
