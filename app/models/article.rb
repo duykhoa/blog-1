f76a520b8e0ff59e5095df2f434dc4e2d89d236c
Sara <rrrrrah@gmail.com>
2015-01-05 21:04:17 -0500

    changed routes, added comments model

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,8 +3,8 @@ class Article < ActiveRecord::Base
 
 	validates :title, presence: true, length: { minimum:1 }
 	validates :thumbnail, presence: true
-	has_attached_file :thumbnail, :styles => {
-		:thumb => "200x200#"
-	}
+	has_attached_file :thumbnail, :styles => { :thumb => "200x200#" }, :default_url => "/images/:style/missing.png"
 	self.per_page = 20
+	validates_attachment_content_type :thumbnail, :content_type => /\Aimage\/.*\Z/
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141216003654) do
+ActiveRecord::Schema.define(version: 20150106002725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 20141216003654) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "article_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
