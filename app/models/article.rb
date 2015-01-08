7a1dea21699581840b70ff9bed6d671897b4367a
Sara <rrrrrah@gmail.com>
2015-01-08 00:37:20 -0500

    added articles and some changes

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,7 +5,7 @@ class Article < ActiveRecord::Base
 	validates :title, presence: true, length: { minimum:1 }
 	validates :thumbnail, presence: true
 	has_attached_file :thumbnail, :styles => { :thumb => "200x200#" }, :default_url => "/images/:style/missing.png"
-	self.per_page = 20
+	self.per_page = 12
 	validates_attachment_content_type :thumbnail, :content_type => /\Aimage\/.*\Z/
 
 end
