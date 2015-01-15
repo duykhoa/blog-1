1380e2fd216222396e4b59278949be1d090c6a0d
Duy Khoa <duykhoa12t@gmail.com>
2015-01-15 13:45:37 +0700

    add contact_mes controller and view form

---
+++ app/controllers/contact_mes_controller.rb
@@ -0,0 +1,4 @@
+class ContactMesController < ApplicationController
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
   resources :articles do
     resources :comments
   end
 
+  resources :contact_mes, only: [:create]
   root 'articles#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
