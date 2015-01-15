bf8645f62d9cfeed9cfd6ea68ed5f2be94812868
Duy Khoa <duykhoa12t@gmail.com>
2015-01-15 11:36:12 +0700

    add published at data for factory and rake task to reset database

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     title Faker::Lorem.sentence
     body Faker::Lorem.paragraph(5)
     published true
+    published_at Time.current
     thumbnail File.new("#{Rails.root}/spec/support/fixtures/thumbnail.jpg")
     association :category
   end
