9f37f3dcbd4fcbd9867f68431c8959e5dc5bd973
lamphuong <halamphuongdct1084@gmail.com>
2015-01-15 14:07:29 +0700

    add admin category

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,18 +1,21 @@
 ActiveAdmin.register Category do
+  permit_params :name
 
+  index do
+    selectable_column
+    id_column
+    column :name
+    actions
+  end
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
+  filter :name
+  filter :created_at
 
+  form do |f|
+    f.inputs "Category Details" do
+      f.input :name
+    end
+    f.actions
+  end
 
 end
