f74f9ca0e6f962cea95f77a93f921f10514dd1cd
Duy Khoa <duykhoa12t@gmail.com>
2015-01-16 09:43:11 +0700

    add reply for comment model

---
+++ db/migrate/20150116024006_create_replies.rb
@@ -0,0 +1,10 @@
+class CreateReplies < ActiveRecord::Migration
+  def change
+    create_table :replies do |t|
+      t.string :content
+      t.integer :comment_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150115045851) do
+ActiveRecord::Schema.define(version: 20150116024006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 20150115045851) do
     t.string   "name"
     t.string   "email"
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "replies", force: true do |t|
+    t.string   "content"
+    t.integer  "comment_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
